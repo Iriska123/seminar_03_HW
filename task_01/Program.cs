a9c317c8661034dccec69402f7e4656d358094cf
Irina <irinapetr1110@mail.ru>
2023-07-28 21:42:03 +0300

    seminar 03

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+(первое == последнему, второе == предпоследнему).
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите 5-ти значное число: ");  //10 000 - 99 999    53635
+int num = int.Parse(Console.ReadLine());
+if (num/10000 < 1 || num/10000 > 9) {
+  Console.WriteLine("Число не пятизначное!");
+  return;
+};
+
+int x1 = num/10000;
+Console.WriteLine("x1 = " + x1);
+int x2 = (num/1000)%10;
+Console.WriteLine("x2 = " + x2);
+
+
+int a = num%100;
+Console.WriteLine("a = " + a);
+
+int y1 = a%10;
+Console.WriteLine("y1 = " + y1);
+int y2 = a/10;
+Console.WriteLine("y2 = " + y2);
+
+if (x1 == y1 && x2 == y2) {
+Console.WriteLine(num + " палиндром");
+} else {
+  Console.WriteLine(num + " НЕ палиндром");
+}
---
+++ task_02/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+
+Console.WriteLine("Введите 1 координату: ");
+int ax = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 координату: ");
+int ay = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 координату: ");
+int az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 4 координату: ");
+int bx = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 5 координату: ");
+int by = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 6 координату: ");
+int bz = int.Parse(Console.ReadLine());
+
+double res = Math.Sqrt( Math.Pow((ax - bx), 2) + Math.Pow((ay - by), 2) + Math.Pow((az - bz), 2));
+Console.WriteLine($"{res:f2}");
+
+
+
+
